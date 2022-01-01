73b7652aefa963107364b79fabc4b7f2b17185d9
Andrew Block <andy.block@gmail.com>
2021-12-31 21:25:36 -0600

    Management of loadbalancer IP

--- test/podman.sh
+++ test/podman.sh
@@ -6,6 +6,12 @@
 #   { "capabilities": { "aliases": true }, "domainName": "dns.podman", "type": "dnsname" }
 # - also make sure the podman host does not override dns hostname in /etc/hosts
 
+# Determine IP Address for Loadbalancer
+echo "Determing IP Address for loadbalancer Container"
+echo "==============================="
+MIN_SUBNET_ADDRESS=$(ipcalc $(podman network inspect loadbalancer | jq -r '.[] | .plugins[] | select(.type == "bridge").ipam.ranges[0][0].subnet') | grep "HostMin" | awk '{ print $2 }')
+LOADBALANCER_IP=$(awk -F\. '{ print $1"."$2"."$3"."$4+1 }' <<< $MIN_SUBNET_ADDRESS )
+
 echo "Updating podman container image"
 echo "==============================="
 podman build -f Containerfile -t ubi8/ubi-ansible-demo:latest
@@ -13,18 +19,12 @@ podman build -f Containerfile -t ubi8/ubi-ansible-demo:latest
 echo "Create podman containers"
 echo "========================"
 echo -n "Starting container loadbalancer0...."
-podman run --name=loadbalancer0 --network loadbalancer --systemd=true  --workdir /work -v $(pwd):/work:rw  -d localhost/ubi8/ubi-ansible-demo:latest /sbin/init
-DNSFILE="/run/containers/cni/dnsname/loadbalancer/addnhosts"
-if [ ! -f "${DNSFILE}" ]; then
-    DNSFILE="/run/user/0/containers/cni/dnsname/loadbalancer/addnhosts"
-fi
+podman run --name=loadbalancer0 -p 80:80 -p 443:443 --ip=$LOADBALANCER_IP --network loadbalancer --systemd=true  --workdir /work -v $(pwd):/work:rw --add-host=loadbalancer0:$LOADBALANCER_IP -d localhost/ubi8/ubi-ansible-demo:latest /sbin/init
 for zone in site1 site2 ; do
     echo "Site: ${zone}"
     while read node ; do
         echo -n "Starting container ${node}.... "
         podman run --name=${node} --network ${zone} --systemd=true  --workdir /work -v $(pwd):/work:rw  -d localhost/ubi8/ubi-ansible-demo:latest /sbin/init
-        # makes loadbalancer visible in sites1 (https://github.com/containers/podman/issues/8399)
-        sleep 2 && podman exec -t ${node} sed -ci "$ a $(cat ${DNSFILE})" /etc/hosts > /dev/null
     done < <( ansible-inventory -i ../scenario --list | jq -r ".${zone}.hosts[]")
 done
 podman network reload --all 2>&1 >/dev/null
